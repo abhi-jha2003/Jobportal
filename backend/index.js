842c1d07d6e13d0721e69dc9a093c3df9053ae99
Abhi Jha <ajha34519@gmail.com>
2024-11-06 22:43:33 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://jobportal2-7gu3.onrender.com",
+  origin: "https://jobportal-e3t3.onrender.com",
   credentials: true,   
 };
 
